a35f4e760d9ef1146ab4448cf84a4411633a93af
Piotr Szmaglinski <pm.szmaglinski@gmail.com>
2017-05-14 08:23:20 +0000

    Repo init

---
+++ main.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+. ./functions
+. ./config.cfg
+
+case $1 in
+add)
+	echo "Creating Amazon environment for user $username"
+
+#########################
+# Create IAM & get keys #
+#########################
+
+addIAMUser $username
+echo "AccessKeyId: ${ACCESSKEYS[0]}"
+echo "SecretAccessKey: ${ACCESSKEYS[1]}"
+
+
+####################
+# Create S3 bucket #
+####################
+
+createS3Bucket $username $s3bucetName
+
+
+###################
+# Create IPA user #
+###################
+
+createIPAUser $username $firstName $lastName $emailAddress "$pubKey"
+
+#############################
+# Create EFS Home directory #
+#############################
+
+createEFSHomedir $username $efsUrl
+
+
+#############################
+# Generate AMI init scripts #
+#############################
+
+generateUserDataFile $username "${ACCESSKEYS[0]}" "${ACCESSKEYS[1]}" $userDataFile $efsUrl $s3bucetName
+
+
+####################
+# Run AMI Instance #
+####################
+
+runInstance $username $userDataFile $amiImgId $instanceType "$secGroups" $vpcSubnetId $iamKeyName
+
+
+###########################
+# Create credentials file #
+###########################
+
+createCredentialsFile $username "${ACCESSKEYS[0]}" "${ACCESSKEYS[1]}" $amiIpAddress $hesServerIpAddress $credFileName
+
+
+
+############################
+# Encrypt credentials file #
+############################
+
+encryptCredentialsFile "$pubKey" $credFileName $encCredFileName
+
+
+
+;;
+
+
+del)
+	echo "Deleteing Amazon environment for user $username"
+
+###################################
+# Delete IAM user and Access Kyes #
+###################################
+
+delIAMUser $username
+
+
+####################
+# Delete S3 Bucket #
+####################
+
+deleteS3Bucket $username $s3bucetName
+
+
+###################
+# Delete IPA user #
+###################
+
+deleteIPAUser $username
+
+
+#############################
+# Delete EFS Home directory #
+#############################
+
+deleteEFSHomedir $username $efsUrl
+
+
+##########################
+# Remove AMI init script #
+##########################
+
+rm -rf ./$userDataFile
+
+
+#######################
+# Remove AMI Instance #
+#######################
+
+terminateInstance $username
+
+
+###########################
+# Remove credentials file #
+###########################
+
+rm -rf ./$credFileName
+
+
+#####################################
+# Remove encrypted credentials file #
+#####################################
+
+rm -rf ./$encCredFileName
+
+;;
+
+
+
+*)
+	echo "Please, choose action (add or del) \$username."
+	exit 1
+
+esac
+	
